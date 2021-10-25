cda3c182692123e88a8b97d92d0ca49d04690b1e
alekskochetov <alekskochetov32@gmail.com>
2021-10-25 06:43:36 -0700

    react route

---
+++ packages/react-router-native/__tests__/link-press-test.tsx
@@ -0,0 +1,151 @@
+import * as React from "react";
+import { Text, TouchableHighlight, View } from "react-native";
+import { Link, NativeRouter, Routes, Route } from "react-router-native";
+import type { ReactTestRenderer } from "react-test-renderer";
+import { act, create as createTestRenderer } from "react-test-renderer";
+
+import { press } from "./utils";
+
+describe("A <Link> press", () => {
+  it("navigates to the new view", () => {
+    function Home() {
+      return (
+        <View>
+          <Text>Home</Text>
+          <Link to="../about">
+            <Text>About</Text>
+          </Link>
+        </View>
+      );
+    }
+
+    function About() {
+      return (
+        <View>
+          <Text>About</Text>
+        </View>
+      );
+    }
+
+    let renderer!: ReactTestRenderer;
+    act(() => {
+      renderer = createTestRenderer(
+        <NativeRouter initialEntries={["/home"]}>
+          <Routes>
+            <Route path="home" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
+        </NativeRouter>
+      );
+    });
+
+    expect(renderer.toJSON()).toMatchSnapshot();
+
+    let touchable = renderer.root.findByType(TouchableHighlight);
+    expect(touchable).not.toBeNull();
+
+    act(() => {
+      press(touchable);
+    });
+
+    expect(renderer.toJSON()).toMatchSnapshot();
+  });
+
+  it("calls the custom onPress handler", () => {
+    let spy = jest.fn();
+
+    function Home() {
+      return (
+        <View>
+          <Text>Home</Text>
+          <Link to="../about" onPress={spy}>
+            <Text>About</Text>
+          </Link>
+        </View>
+      );
+    }
+
+    function About() {
+      return (
+        <View>
+          <Text>About</Text>
+        </View>
+      );
+    }
+
+    let renderer!: ReactTestRenderer;
+    act(() => {
+      renderer = createTestRenderer(
+        <NativeRouter initialEntries={["/home"]}>
+          <Routes>
+            <Route path="home" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
+        </NativeRouter>
+      );
+    });
+
+    expect(renderer.toJSON()).toMatchSnapshot();
+
+    let touchable = renderer.root.findByType(TouchableHighlight);
+    expect(touchable).not.toBeNull();
+
+    let pressEvent;
+    act(() => {
+      pressEvent = press(touchable);
+    });
+
+    expect(spy).toHaveBeenCalledWith(pressEvent);
+  });
+
+  describe("when event.preventDefault() is used in the onPress handler", () => {
+    it("does not navigate to the new view", () => {
+      function Home() {
+        return (
+          <View>
+            <Text>Home</Text>
+            <Link
+              to="../about"
+              onPress={event => {
+                event.preventDefault();
+              }}
+            >
+              <Text>About</Text>
+            </Link>
+          </View>
+        );
+      }
+
+      function About() {
+        return (
+          <View>
+            <Text>About</Text>
+          </View>
+        );
+      }
+
+      let renderer!: ReactTestRenderer;
+      act(() => {
+        renderer = createTestRenderer(
+          <NativeRouter initialEntries={["/home"]}>
+            <Routes>
+              <Route path="home" element={<Home />} />
+              <Route path="about" element={<About />} />
+            </Routes>
+          </NativeRouter>
+        );
+      });
+
+      expect(renderer.toJSON()).toMatchSnapshot();
+
+      let touchable = renderer.root.findByType(TouchableHighlight);
+      expect(touchable).not.toBeNull();
+
+      act(() => {
+        press(touchable);
+      });
+
+      expect(renderer.toJSON()).toMatchSnapshot();
+    });
+  });
+});
